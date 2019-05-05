e803e82321034e766dac4fce15161e679001aa22
Carlos <carlosm@yzag.com>
2019-05-05 17:25:43 -0400

    modify this branch just for read data from memory

--- src/app/services/devices.service.ts
+++ src/app/services/devices.service.ts
@@ -1,33 +1,22 @@
 import { Injectable } from '@angular/core';
-import {environment} from '../../environments/environment';
-import {HttpClient} from '@angular/common/http';
 import {MemClientService} from './mem-client.service';
 
 @Injectable()
 export class DevicesService {
 
-  private url: string;
-  constructor(private http: HttpClient, private mem: MemClientService) {
-    this.url = !this.isMemoryEnv ? `${environment.apiUrl}/devices` : '';
-  }
-
-  private get isMemoryEnv() {
-    return environment.name === 'memory';
+  constructor(private mem: MemClientService) {
   }
 
   public deviceById(deviceId) {
-    const urlParam = `${this.url}/${deviceId}`;
-    return !this.isMemoryEnv ? this.http.get(urlParam) : this.mem.deviceById(deviceId);
+    return this.mem.deviceById(deviceId);
   }
 
   public edit(deviceId, data) {
-    const urlParam = `${this.url}/${deviceId}`;
-    return !this.isMemoryEnv ? this.http.put(urlParam, data) : this.mem.editDevice(deviceId, data);
+    return this.mem.editDevice(deviceId, data);
   }
 
   public remove(deviceId) {
-    const urlParam = `${this.url}/${deviceId}`;
-    return !this.isMemoryEnv ? this.http.delete(urlParam) : this.mem.removeDevice(deviceId);
+    return this.mem.removeDevice(deviceId);
   }
 
 }
--- src/app/services/gateways.service.ts
+++ src/app/services/gateways.service.ts
@@ -1,44 +1,31 @@
 import { Injectable } from '@angular/core';
-import {environment} from '../../environments/environment';
-import {HttpClient} from '@angular/common/http';
 import {MemClientService} from './mem-client.service';
 @Injectable()
 export class GatewaysService {
 
-  private url: string;
-  constructor(private http: HttpClient, private mem: MemClientService) {
-    this.url = !this.isMemoryEnv ? `${environment.apiUrl}/gateways` : '';
-  }
-
-  private get isMemoryEnv() {
-    return environment.name === 'memory';
-  }
+  constructor(private mem: MemClientService) {}
 
   get list() {
-    return !this.isMemoryEnv ? this.http.get(this.url) : this.mem.gatewaylist();
+    return this.mem.gatewaylist();
   }
 
   public gatewayById(gatewayId) {
-    const urlParam = `${this.url}/${gatewayId}`;
-    return !this.isMemoryEnv ? this.http.get(urlParam) : this.mem.gatewayById(gatewayId);
+    return this.mem.gatewayById(gatewayId);
   }
 
   public add(data) {
-    return !this.isMemoryEnv ? this.http.post(this.url, data) : this.mem.addGateway(data);
+    return this.mem.addGateway(data);
   }
 
   public edit(gatewayId, data) {
-    const urlParam = `${this.url}/${gatewayId}`;
-    return !this.isMemoryEnv ? this.http.put(urlParam, data) : this.mem.editGateway(gatewayId, data);
+    return this.mem.editGateway(gatewayId, data);
   }
 
   public deviceList(gatewayId) {
-    const urlParam = `${this.url}/${gatewayId}/devices`;
-    return !this.isMemoryEnv ? this.http.get(urlParam) : this.mem.deviceList(gatewayId);
+    return this.mem.deviceList(gatewayId);
   }
 
   public addDevice(gatewayId, data) {
-    const urlParam = `${this.url}/${gatewayId}/device`;
-    return !this.isMemoryEnv ? this.http.post(urlParam, data) : this.mem.addDevice(gatewayId,data);
+    return this.mem.addDevice(gatewayId, data);
   }
 }
